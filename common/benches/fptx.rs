7af592b8f9c7442de30e54077b6514fcbc23d96b
Rex Fernando <rex@nnphy.me>
2026-03-09 15:53:24 +0000

    typos

--- common/benches/fptx.rs
+++ common/benches/fptx.rs
@@ -18,11 +18,13 @@ fn main() {
     let (new_contrib, _) = FPTXContributionInner::generate(&mut rng, &first_contrib, &params);
     println!("Time to generate a user contrib: {:?}", time.elapsed());
 
+    let time = std::time::Instant::now();
     println!("Starting user contrib serialization at {}", chrono::Local::now() );
     fs::write("./test.contrib", bcs::to_bytes(&new_contrib).unwrap()).unwrap();
     println!("Time to serialize a user contrib: {:?}", time.elapsed());
     println!("Size: {} MB", fs::metadata("./test.contrib").unwrap().len()/1000/1000);
     
+    let time = std::time::Instant::now();
     println!("Starting user contrib deserialization at {}", chrono::Local::now() );
     let new_contrib : FPTXContributionInner = bcs::from_reader(fs::File::open("./test.contrib").unwrap()).unwrap();
     println!("Time to deserialize a user contrib: {:?}", time.elapsed());
