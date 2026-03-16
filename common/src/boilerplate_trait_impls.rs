bea4ca60d70f4837a94f15f99e5737bb68ac283b
Rex Fernando <rex@nnphy.me>
2026-03-16 19:52:18 +0000

    Switch struct representation to Affine instead of Projective

--- common/src/boilerplate_trait_impls.rs
+++ common/src/boilerplate_trait_impls.rs
@@ -1,7 +1,9 @@
 //! The only reason for this file is that we don't want to require M2C to be
 //! `{Debug,Clone,PartialEq,Eq}` in order to implement the respective traits for our structs that
 //! are parameterized by it, and the default derive macros have this requirement.
-use ark_ec::{CurveGroup, hashing::map_to_curve_hasher::MapToCurve, pairing::Pairing};
+use std::ops::Neg;
+
+use ark_ec::{AffineRepr, CurveGroup, hashing::map_to_curve_hasher::MapToCurve, pairing::Pairing};
 
 use crate::{bls_sok::BLSSoK, powers_of_tau::PowersOfTauContributionInner};
 
@@ -9,8 +11,8 @@ impl<P: Eq, M2C> Eq for BLSSoK<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G2: CurveGroup,
-    P::G1: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
 }
 
@@ -18,8 +20,8 @@ impl<P: PartialEq, M2C> PartialEq for BLSSoK<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G2: CurveGroup,
-    P::G1: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
     fn eq(&self, other: &Self) -> bool {
         self.sig == other.sig 
@@ -30,8 +32,8 @@ impl<P: Clone, M2C> Clone for BLSSoK<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G2: CurveGroup,
-    P::G1: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
     fn clone(&self) -> Self {
         Self { sig: self.sig.clone(), _phantom: self._phantom.clone() }
@@ -42,8 +44,8 @@ impl<P: std::fmt::Debug, M2C> std::fmt::Debug for BLSSoK<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G2: CurveGroup,
-    P::G1: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("BLSSoK").field("sig", &self.sig).field("_phantom", &self._phantom).finish()
@@ -54,8 +56,8 @@ impl<P: Eq, M2C> Eq for PowersOfTauContributionInner<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G1: CurveGroup,
-    P::G2: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
 }
 
@@ -63,8 +65,8 @@ impl<P: PartialEq, M2C> PartialEq for PowersOfTauContributionInner<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G1: CurveGroup,
-    P::G2: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
     fn eq(&self, other: &Self) -> bool {
         self.powers == other.powers && self.tau_g2 == other.tau_g2 && self.sok == other.sok
@@ -75,8 +77,8 @@ impl<P: Clone, M2C> Clone for PowersOfTauContributionInner<P, M2C>
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G1: CurveGroup,
-    P::G2: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
     fn clone(&self) -> Self {
         Self { powers: self.powers.clone(), tau_g2: self.tau_g2.clone(), sok: self.sok.clone() }
@@ -87,8 +89,8 @@ impl<P: std::fmt::Debug, M2C> std::fmt::Debug for PowersOfTauContributionInner<P
 where
     P: Pairing,
     M2C: MapToCurve<P::G1>,
-    P::G1: CurveGroup,
-    P::G2: CurveGroup,
+    P::G2Affine: AffineRepr + Neg<Output = P::G2Affine>,
+    P::G1Affine: AffineRepr,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("PowersOfTauContributionInner").field("powers", &self.powers).field("tau_g2", &self.tau_g2).field("sok", &self.sok).finish()
--- common/src/multipairing_equation.rs
+++ common/src/multipairing_equation.rs
@@ -1,8 +1,13 @@
-use ark_ec::{VariableBaseMSM, pairing::{Pairing, PairingOutput}};
+use ark_ec::{
+    VariableBaseMSM,
+    pairing::{Pairing, PairingOutput},
+};
 use ark_ff::Zero;
 use ark_std::UniformRand;
 use rand_core::CryptoRngCore;
-use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
+};
 
 use crate::errors::MultipairingEquationNonZeroResult;
 
@@ -10,8 +15,9 @@ pub struct MultipairingEquation<P>
 where
     P: Pairing,
 {
-    g1s: Vec<P::G1>,
-    g2s: Vec<P::G2>,
+    // note: this struct is never serialized, so normalization doesn't happen
+    g1s: Vec<P::G1Affine>,
+    g2s: Vec<P::G2Affine>,
 }
 
 impl<P> MultipairingEquation<P>
@@ -20,40 +26,48 @@ where
 {
     /// Construct a new multipairing equation. Takes vecs instead of slices so that we can move w/o
     /// calling `to_vec()`, which I believe does a copy.
-    pub fn simple(g1s: Vec<P::G1>, g2s: Vec<P::G2>) -> Self {
+    pub fn simple(g1s: Vec<P::G1Affine>, g2s: Vec<P::G2Affine>) -> Self {
         Self { g1s, g2s }
     }
 
-    pub fn with_shared_g2s(rng: &mut impl CryptoRngCore, g1s: Vec<Vec<P::G1>>, shared_g2s: Vec<P::G2>) -> Self {
+    pub fn with_shared_g2s(
+        rng: &mut impl CryptoRngCore,
+        g1s: Vec<Vec<P::G1Affine>>,
+        shared_g2s: Vec<P::G2Affine>,
+    ) -> Self {
         assert!(g1s.len() > 0);
 
-        let random_challenge : Vec<P::ScalarField> = 
-        (0..g1s.len()).map(|_| P::ScalarField::rand(rng)).collect();
+        let random_challenge: Vec<P::ScalarField> =
+            (0..g1s.len()).map(|_| P::ScalarField::rand(rng)).collect();
 
-        let g1s_combined_scaled : Vec<P::G1> = (0..g1s[0].len()).map(|i|
-            <P::G1 as VariableBaseMSM>::msm_unchecked(
-                &g1s.iter().map(|g1s| P::G1Affine::from(g1s[i])).collect::<Vec<P::G1Affine>>(),
-                &random_challenge
-            ))
+        // I believe that having g1s,g2s be G1 instead of G1Affine means less work here since we
+        // don't have to normalize.
+        let g1s_combined_scaled: Vec<P::G1Affine> = (0..g1s[0].len())
+            .map(|i| {
+                <P::G1 as VariableBaseMSM>::msm_unchecked(
+                    &g1s.iter()
+                        .map(|g1s| g1s[i])
+                        .collect::<Vec<P::G1Affine>>(),
+                    &random_challenge,
+                ).into()
+            })
             .collect();
 
         Self {
             g1s: g1s_combined_scaled,
             g2s: shared_g2s,
         }
-
     }
 
     pub fn empty() -> Self {
         Self::simple(vec![], vec![])
     }
 
-
     fn scalar_mul(self, scalar: P::ScalarField) -> Self {
         Self {
-            g1s: self.g1s.into_par_iter().map( |g1| g1 * scalar ).collect(),
+            g1s: self.g1s.into_par_iter().map(|g1| P::G1Affine::from(g1 * scalar)).collect(),
             // I believe the move and the use of `self` instead of `&self` above means no copy here
-            g2s: self.g2s
+            g2s: self.g2s,
         }
     }
 
@@ -71,8 +85,16 @@ where
     pub fn equals_zero(&self) -> Result<(), MultipairingEquationNonZeroResult> {
         println!("multipairing size: {}", self.g1s.len());
         let time = std::time::Instant::now();
-        let g1s_prepared: Vec<P::G1Prepared> = self.g1s.par_iter().map(|g| P::G1Prepared::from(*g)).collect();
-        let g2s_prepared: Vec<P::G2Prepared> = self.g2s.par_iter().map(|g| P::G2Prepared::from(*g)).collect();
+        let g1s_prepared: Vec<P::G1Prepared> = self
+            .g1s
+            .par_iter()
+            .map(|g| P::G1Prepared::from(*g))
+            .collect();
+        let g2s_prepared: Vec<P::G2Prepared> = self
+            .g2s
+            .par_iter()
+            .map(|g| P::G2Prepared::from(*g))
+            .collect();
         let output = P::multi_pairing(g1s_prepared, g2s_prepared);
         println!("equals_zero: {:?}", time.elapsed());
 
@@ -85,14 +107,12 @@ where
 }
 
 pub struct MultipairingEquations<P: Pairing> {
-    pub eqns : Vec<MultipairingEquation<P>>,
+    pub eqns: Vec<MultipairingEquation<P>>,
 }
 
 impl<P: Pairing> MultipairingEquations<P> {
     pub fn new() -> Self {
-        Self { 
-            eqns: vec![]
-        }
+        Self { eqns: vec![] }
     }
 
     pub fn add(mut self, eq: MultipairingEquation<P>) -> Self {
@@ -113,13 +133,16 @@ impl<P: Pairing> MultipairingEquations<P> {
             random_scalars.push(P::ScalarField::rand(rng));
         }
 
-        let (g1s, g2s): (Vec<<P as Pairing>::G1>, Vec<<P as Pairing>::G2>) = self.eqns
+        let (g1s, g2s): (Vec<<P as Pairing>::G1Affine>, Vec<<P as Pairing>::G2Affine>) = self
+            .eqns
             .into_par_iter()
             .zip(random_scalars)
-            .map(|(eq, scalar)| { 
+            .map(|(eq, scalar)| {
                 let new_eq = eq.scalar_mul(scalar);
                 (new_eq.g1s, new_eq.g2s)
-            }).flatten().collect();
+            })
+            .flatten()
+            .collect();
 
         println!("compact: {:?}", time.elapsed());
 
@@ -127,10 +150,10 @@ impl<P: Pairing> MultipairingEquations<P> {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use aptos_batch_encryption::group::{Fr, G1Projective, G2Projective, Pairing};
+    use aptos_batch_encryption::group::{Fr, G1Projective, G2Projective, Pairing, G1Affine, G2Affine};
+    use ark_ec::AffineRepr as _;
     use ark_ec::PrimeGroup;
     use ark_std::UniformRand;
     use ark_std::Zero;
@@ -151,16 +174,22 @@ mod tests {
             frs_2.push(Fr::rand(&mut rng));
         }
 
-        let partial_sum : Fr = frs_2.iter().zip(&frs_1).map(|(x,y)| *x*y).sum();
-        frs_2.push(-partial_sum/frs_1[3]);
+        let partial_sum: Fr = frs_2.iter().zip(&frs_1).map(|(x, y)| *x * y).sum();
+        frs_2.push(-partial_sum / frs_1[3]);
 
         assert_eq!(
-            frs_2.iter().zip(&frs_1).map(|(x,y)| *x*y).sum::<Fr>(),
+            frs_2.iter().zip(&frs_1).map(|(x, y)| *x * y).sum::<Fr>(),
             Fr::zero()
         );
 
-        let g1s : Vec<G1Projective> = frs_1.into_iter().map(|x| G1Projective::generator()*x).collect();
-        let g2s : Vec<G2Projective> = frs_2.into_iter().map(|x| G2Projective::generator()*x).collect();
+        let g1s: Vec<G1Affine> = frs_1
+            .into_iter()
+            .map(|x| G1Affine::from(G1Affine::generator() * x))
+            .collect();
+        let g2s: Vec<G2Affine> = frs_2
+            .into_iter()
+            .map(|x| G2Affine::from(G2Affine::generator() * x))
+            .collect();
 
         MultipairingEquation::simple(g1s, g2s)
     }
@@ -178,15 +207,16 @@ mod tests {
             frs_2.push(Fr::rand(&mut rng));
         }
 
+        assert!(frs_2.iter().zip(&frs_1).map(|(x, y)| *x * y).sum::<Fr>() != Fr::zero());
 
-        assert!(
-            frs_2.iter().zip(&frs_1).map(|(x,y)| *x*y).sum::<Fr>()
-            !=
-            Fr::zero()
-        );
-
-        let g1s : Vec<G1Projective> = frs_1.into_iter().map(|x| G1Projective::generator()*x).collect();
-        let g2s : Vec<G2Projective> = frs_2.into_iter().map(|x| G2Projective::generator()*x).collect();
+        let g1s: Vec<G1Affine> = frs_1
+            .into_iter()
+            .map(|x| G1Affine::from(G1Affine::generator() * x))
+            .collect();
+        let g2s: Vec<G2Affine> = frs_2
+            .into_iter()
+            .map(|x| G2Affine::from(G2Affine::generator() * x))
+            .collect();
 
         MultipairingEquation::simple(g1s, g2s)
     }
@@ -253,4 +283,3 @@ mod tests {
         eq1.combine(&mut rng, eq2).equals_zero().unwrap();
     }
 }
-
