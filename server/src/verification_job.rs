e46697dd532ab8514d43c4bcf242cf1c2425fd95
Rex Fernando <rex@nnphy.me>
2026-04-03 20:09:10 +0000

    better logging

--- server/src/verification_job.rs
+++ server/src/verification_job.rs
@@ -6,6 +6,7 @@ use rand::thread_rng;
 use rayon;
 use anyhow::{Context, Result};
 use tokio::sync::oneshot;
+use tracing::info;
 
 use crate::store::contribution_files::ContributionFilesStore;
 
@@ -26,27 +27,39 @@ impl VerificationJob {
 
         let start = Instant::now();
         let current_contribution_hash = blake3::hash(&current_contribution_bytes);
-        println!("time taken to hash: {:?}", start.elapsed());
+        info!("time taken to hash: {:?}", start.elapsed());
 
         if current_contribution_hash.to_string() != hash {
             anyhow::bail!("Contribution hash mismatch");
         }
 
+        info!("{}: Starting deserialization", current.name);
+        let start = Instant::now();
         let current_contribution : CeremonyContribution = bcs::from_bytes(&current_contribution_bytes)
         .context("Error while parsing the uploaded contribution")?;
+        info!("{}: Finished deserialization, time taken: {:?}", current.name, start.elapsed());
+
+        info!("{}: Starting deserialization of previous", current.name);
+        let start = Instant::now();
         let maybe_previous_contribution : Option<CeremonyContribution> = match maybe_previous {
             Some(previous) => Some(bcs::from_bytes(&contribution_files_store.download_contribution(&previous).await?)
                 .context("Error while parsing the previous contribution")?),
             None => None
         };
+        info!("{}: Finished deserialization of previous, time taken: {:?}", current.name, start.elapsed());
 
         let (tx, rx) = oneshot::channel();
         let params = params.clone();
 
+        // to avoid moving current into the closure
+        let name_cloned = current.name.clone();
 
         rayon::spawn(move || {
             let mut rng = thread_rng();
+            info!("{}: Starting verification computation", name_cloned);
+            let start = Instant::now();
             let verification_result = current_contribution.verify(&mut rng, maybe_previous_contribution.as_ref(), &params);
+            info!("{}: Finished verification computation, time taken: {:?}", name_cloned, start.elapsed());
             tx.send(verification_result)
             .expect("Sending should always succeed")
         });
