3546b7c496320cc91c8d4e167d5e7020cd3da3af
Rex Fernando <rex@nnphy.me>
2026-04-20 17:29:09 +0000

    typo

--- common/src/powers_of_tau.rs
+++ common/src/powers_of_tau.rs
@@ -121,7 +121,7 @@ where
         if self.powers.len() != params.max_power + 1 {
             return Err(ContributionVerificationFailure::ParamsMismatch);
         }
-        if self.powers[0] != P::G1Affine::zero() {
+        if self.powers[0] != P::G1Affine::generator() {
             return Err(ContributionVerificationFailure::ShapeMismatch);
         }
         if self.tau_g2 == P::G2Affine::zero() {
